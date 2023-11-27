c52224ddf38b614f0f40b5ce72315512d9fb54f5
rijwanulalam <rijwanul007@gmail.com>
2023-11-27 14:33:51 +0600

    modal fixed for large device

--- js/app.js
+++ js/app.js
@@ -15,20 +15,24 @@ const showData = (tools) => {
   fewTools.forEach((tools) => {
     console.log(tools);
     // date formatting
-    const date = (tools.published_in);
-    [month, day, year] = date.split('/');
+    const date = tools.published_in;
+    [month, day, year] = date.split("/");
     const newDate = [day, month, year];
-    const newFormat = (newDate.join("/"))
+    const newFormat = newDate.join("/");
 
     const postDiv = document.createElement("div");
 
     postDiv.classList.add("p-5", "border", "rounded-lg");
     postDiv.innerHTML = `
-        <img class="h-72 w-96 rounded-lg" src="${tools?.image}" alt="${tools.name}">
+        <img class="h-72 w-96 rounded-lg" src="${tools?.image}" alt="${
+      tools.name
+    }">
         <h3 class="text-xl font-bold my-5 uppercase">Features</h3>
         <li class="list-decimal">${tools.features[0]}</li>
         <li class="list-decimal">${tools.features[1]}</li>
-        <li class="list-decimal">${tools.features[2] ? tools.features[2] : "Text generation"}</li>
+        <li class="list-decimal">${
+          tools.features[2] ? tools.features[2] : "Text generation"
+        }</li>
         <hr class="my-5">
         <div class="flex items-center justify-between">
             <div>
@@ -38,7 +42,9 @@ const showData = (tools) => {
                 </span>${newFormat}</p>
             </div>
             <div>
-                <span onclick="handleModal('${tools.id}')" class="material-symbols-outlined text-green-700 cursor-pointer">
+                <span onclick="handleModal('${
+                  tools.id
+                }')" class="material-symbols-outlined text-green-700 cursor-pointer">
                 arrow_forward
                 </span>
             </div>
@@ -48,23 +54,45 @@ const showData = (tools) => {
   });
 };
 
-const handleModal = async(id) => {
-   const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
-   const res = await fetch(url);
-   const data = await res.json();
-   showModal(data.data)
-}
+const handleModal = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  showModal(data.data);
+};
 
-const showModal = infos => {
-    console.log(infos)
-    const divModal = document.getElementById("show-modal");
-    divModal.classList.remove("hidden");
-    const gridDiv = document.createElement("div");
-    gridDiv.classList.add("grid", "gap-10", "grid-cols-2", "bg-white", "rounded", "shadow-lg", "p-5", "w-9/12");
-    gridDiv.innerHTML = `
-    <div class="p-10 border-2 border-pink-400 rounded-lg bg-pink-100/50">
+const showModal = (infos) => {
+  console.log(infos);
+  // pricing split
+  const contactUs = infos.pricing[2].price.split(" ");
+  // console.log(contactUs[0]+ " " + contactUs[1])
+
+  const divModal = document.getElementById("show-modal");
+  divModal.innerHTML = "";
+  divModal.classList.remove("hidden");
+  const gridDiv = document.createElement("div");
+  gridDiv.classList.add(
+    "grid",
+    "grid-cols-1",
+    "gap-10",
+    "md:grid-cols-2",
+    "bg-white",
+    "rounded",
+    "shadow-lg",
+    "md:p-10",
+    "w-9/12",
+    "relative"
+  );
+  gridDiv.innerHTML = `
+        <div class=" absolute top-0 right-0">
+        <span id="close-modal" class="material-symbols-outlined text-red-700 cursor-pointer bg-red-100 rounded text-4xl font-bold">
+        close
+        </span>
+      </div>
+    <div class="md:p-10 border-2 border-pink-400 rounded-lg bg-pink-100/50">
+    
         <h3 class="text-3xl font-bold">${infos.description}</h3>
-        <div class="grid grid-cols-3 my-10 gap-10 text-xl font-medium">
+        <div class="grid grid-cols-1 md:grid-cols-3 my-10 gap-10 text-xl font-medium">
             <div class="text-center bg-white p-10 rounded-lg text-green-500">
                 <h3>${infos.pricing[0].price}</h3>
                 <h3>${infos.pricing[0].plan}</h3>
@@ -74,7 +102,7 @@ const showModal = infos => {
                 <h3>${infos.pricing[1].plan}</h3>
             </div>
             <div class="text-center bg-white p-10 rounded-lg text-pink-500">
-                <h3>${infos.pricing[2].price}</h3>
+                <h3>${contactUs[0] + " " + contactUs[1]}</h3>
                 <h3>${infos.pricing[2].plan}</h3>
             </div>
         </div>
@@ -95,11 +123,26 @@ const showModal = infos => {
     </div>
     <div class="text-center p-10 border rounded-lg">
       <img class=" rounded-lg" src="${infos.image_link[0]}" alt="">
-      <h3 class="text-4xl font-bold my-2">${infos.input_output_examples ? infos.input_output_examples[0].input : infos.input_output_examples[1].input}</h3>
-      <p class="text-xl">${infos.input_output_examples ? infos.input_output_examples[0].output : infos.input_output_examples[1].output}</p>
+      <h3 class="text-4xl font-bold my-2">${
+        infos.input_output_examples
+          ? infos.input_output_examples[0].input
+          : infos.input_output_examples[1].input
+      }</h3>
+      <p class="text-xl">${
+        infos.input_output_examples
+          ? infos.input_output_examples[0].output
+          : infos.input_output_examples[1].output
+      }</p>
     </div>
-    `
-    divModal.appendChild(gridDiv);
-}
+    `;
+  divModal.appendChild(gridDiv);
+
+  document.getElementById("close-modal").addEventListener("click", function(){
+    const modal = document.getElementById("show-modal");
+    modal.classList.add("hidden");
+  })
+  
+};
+
 
 loadData();
